db1d0f8b2b7bba40d897744d7ce004185cc3af9c
LLQ <1121049757@qq.com>
2021-02-18 15:31:45 +0800

    update

---
+++ 网易严选商城/mall-server/img.sql
@@ -0,0 +1,14 @@
+use shop;
+
+update nideshop_channel set icon_url='https://yanxuan.nosdn.127.net/fede8b110c502ec5799702d5ec824792.png'
+where id = '1';
+update nideshop_channel set icon_url='https://yanxuan.nosdn.127.net/37520d1204a0c55474021b43dac2a69e.png'
+where id ='2';
+update nideshop_channel set icon_url='https://yanxuan.nosdn.127.net/6c3bd9d885c818b1f73e497335a68b47.png'
+where id ='3';
+update nideshop_channel set icon_url='https://yanxuan.nosdn.127.net/896a3beac514ae8f40aafe028e5fec56.png'
+where id ='4';
+update nideshop_channel set icon_url='https://yanxuan.nosdn.127.net/6c3bd9d885c818b1f73e497335a68b47.png'
+where id ='5';
+
+select * from nideshop_channel;
